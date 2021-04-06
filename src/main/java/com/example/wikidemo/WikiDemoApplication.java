de8ba76b974ea40e48603fb0576ee6a4d195837b
shawn <317396226@qq.com>
2021-04-06 23:34:25 +0800

    2-2

---
+++ src/main/java/com/example/wikidemo/WikiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.wikidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiDemoApplication.class, args);
+    }
+
+}
