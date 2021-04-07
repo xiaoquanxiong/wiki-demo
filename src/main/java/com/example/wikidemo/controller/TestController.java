ee02027aff9a52d269e6591a216c5c6a632a14d6
shawn <317396226@qq.com>
2021-04-07 23:48:41 +0800

    2-7 使用 http client 测试接口

--- src/main/java/com/example/wikidemo/controller/TestController.java
+++ src/main/java/com/example/wikidemo/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.wikidemo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
     }
 }
