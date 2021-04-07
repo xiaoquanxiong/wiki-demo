dc371e982a0f3af58c11c8074aca17718d979e40
shawn <317396226@qq.com>
2021-04-08 06:52:17 +0800

    2-8 springboot配置文件

--- src/main/java/com/example/wikidemo/controller/TestController.java
+++ src/main/java/com/example/wikidemo/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.example.wikidemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+    
+    @Value("${test.hello:TEST}")
+    private String testHello;
+    
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello " + testHello;
     }
 
     @PostMapping("/hello/post")
