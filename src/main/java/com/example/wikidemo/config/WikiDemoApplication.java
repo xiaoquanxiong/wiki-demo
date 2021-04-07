f02d331332ca37b303264cca64b207b5ad176012
shawn <317396226@qq.com>
2021-04-07 23:26:20 +0800

    2-5，6 优化启动日志；项目分层，启动类换位置

---
+++ src/main/java/com/example/wikidemo/config/WikiDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.wikidemo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+@ComponentScan({"com.example"})
+@SpringBootApplication
+public class WikiDemoApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(WikiDemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(WikiDemoApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
