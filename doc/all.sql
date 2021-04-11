c7eef5c97d1eb6f0f5b6f518b541d61c4d42d62b
shawn <317396226@qq.com>
2021-04-11 11:30:51 +0800

    3-5 集成mybatis generator, 生成 Demo 表代码

--- doc/all.sql
+++ doc/all.sql
@@ -7,3 +7,14 @@ CREATE TABLE `test` (
 ) engine=innodb DEFAULT charset=utf8mb4 comment='测试';
 
 INSERT into `test` (id, name, password) values (1, '测试', 'password');
+
+
+
+DROP TABLE IF EXISTS `demo`;
+CREATE TABLE `demo` (
+    `id` bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key(`id`)
+) engine=innodb DEFAULT charset=utf8mb4 comment='测试';
+
+INSERT INTO `demo` (id, name) VALUES (1, '测试');
