873d4abecdfcdae7c97b16959157c861b4b6e6e2
shawn <317396226@qq.com>
2021-04-11 18:33:15 +0800

    3-5 增加demo表的查询，service、controller类

---
+++ src/main/java/com/example/wikidemo/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.example.wikidemo.service;
+
+
+import com.example.wikidemo.domain.Demo;
+import com.example.wikidemo.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
