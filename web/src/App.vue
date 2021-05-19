bd7e31b9a9a1bf6865795859e7bdcfbf0f104dc2
shawn <317396226@qq.com>
2021-05-19 16:25:56 +0800

    4-7 制作 the-footer 组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      Ant Design ©2018 Created by Ant UED
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue'; // @ is an alias to /src
+import TheFooter from '@/components/the-footer.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter
   },
 });
 </script>
