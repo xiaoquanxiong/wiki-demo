0628ab9b44294adfc597b583b9c64e68f4ce053c
shawn <317396226@qq.com>
2021-05-21 08:52:40 +0800

    5-4 引入AntD的Icon依赖，实现Icon一次性全部Import

--- web/src/main.ts
+++ web/src/main.ts
@@ -4,9 +4,15 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
 
-createApp(App).
-    use(store).
+const app = createApp(App)
+app.use(store).
     use(router).
     use(Antd).
     mount('#app')
+//全局使用图标
+const icons:any = Icons;
+for (const i in icons){
+    app.component(i, icons[i]);
+}
